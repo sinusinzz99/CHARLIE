566fa8d25d93dce39a95a73033165891ccba013b
sinusinzz99 <91043579+sinusinzz99@users.noreply.github.com>
2021-09-20 10:58:11 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,6 @@
-/* Copyright (C) 2020 Yusuf Usta.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-WhatsAsena - Yusuf Usta
-*/
 
-const fs = require("fs");
-const path = require("path");
-const events = require("./events");
-const chalk = require('chalk');
 const config = require('./config');
-const {WAConnection, MessageType, Presence} = require('@adiwajshing/baileys');
+st {WACnection, MessageType, Presence} = require('@adiwajshing/baileys');
 const {Message, StringSession, Image, Video} = require('./whatsasena/');
 const { DataTypes } = require('sequelize');
 const { getMessage } = require("./plugins/sql/greetings");
